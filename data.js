d1706fed7da2b8ddb89378208499072d7e012399
krishnanayak42 <78615766+krishnanayak42@users.noreply.github.com>
2025-03-28 18:21:51 +0000

    new

---
+++ data.js
@@ -0,0 +1,12 @@
+const krishna=848
+let ram="dfkjdjkf"
+var shiva="jjhjh"
+sita="hjhjh"//not to be use that type method ok
+let a
+console.log(krishna)
+console.table([krishna,ram,shiva,sita,a]);
+
+/*
+perfer not use var 
+because of issue in block scope and function scope
+*/
